72f35aed34d38347086102a0a78c508acfeb865e
sfsheng0322 <sfsheng0322@gmail.com>
2016-07-26 14:27:48 +0800

    modify module

--- app/src/main/java/com/sunfusheng/small/LaunchActivity.java
+++ app/src/main/java/com/sunfusheng/small/LaunchActivity.java
@@ -12,7 +12,6 @@ public class LaunchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch);
-
     }
 
     @Override
